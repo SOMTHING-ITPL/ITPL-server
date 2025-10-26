9edce2272584302f2178e0ea737c1bd6904031fd
King_Eon <0302tjddjs@gmail.com>
2025-10-26 21:44:04 +0900

    additional user info in ChatMessageInfo response(profile image, nickname)

--- internal/handler/chatroom_handler.go
+++ internal/handler/chatroom_handler.go
@@ -28,7 +28,7 @@ func NewChatRoomHandler(chatRoomRepo *chat.ChatRoomRepository, userRepo *user.Re
 // only title search
 func (h *ChatRoomHandler) GetChatRoomsByTitle() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		title := c.Param("title")
+		title := c.Query("title")
 		rooms, err := h.chatRoomRepository.SearchChatRoomsByTitle(title)
 		if err != nil {
 			c.Status(http.StatusInternalServerError)
@@ -214,8 +214,7 @@ func (h *ChatRoomHandler) JoinChatRoom() gin.HandlerFunc {
 			c.Status(http.StatusInternalServerError)
 			return
 		}
-		c.JSON(http.StatusOK, CommonRes{
-			Message: "Joined chat room successfully"})
+		c.Status(http.StatusNoContent)
 	}
 }
 
@@ -329,10 +328,20 @@ func (h *ChatRoomHandler) GetHistory() gin.HandlerFunc {
 			c.Status(http.StatusInternalServerError)
 			return
 		}
+		var response []ChatMessageResponse
+		for _, message := range messages {
+			res, err := ToChatMessageResponse(h.bucketBasics.AwsConfig, h.bucketBasics.BucketName, h.chatRoomRepository.DB, message)
+			if err != nil {
+				log.Printf("Failed to get chat room member info: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get chat room member info"})
+				return
+			}
+			response = append(response, res)
+		}
 
 		c.JSON(http.StatusOK, CommonRes{
 			Message: "success",
-			Data:    messages,
+			Data:    response,
 		})
 	}
 }
